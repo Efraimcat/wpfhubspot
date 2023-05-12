4210bac937f74946d63433f9eb1c26a7339153a1
efraimcat <efraim@efraim.cat>
2023-05-12 12:11:36 +0200

    V1.0.0 12-05-23 12:11

---
+++ includes/class-wpfhubspot-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://efraim.cat
+ * @since      1.0.0
+ *
+ * @package    Wpfhubspot
+ * @subpackage Wpfhubspot/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wpfhubspot
+ * @subpackage Wpfhubspot/includes
+ * @author     Efraim Bayarri <efraim@efraim.cat>
+ */
+class Wpfhubspot_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
